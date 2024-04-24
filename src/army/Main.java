00cb0971e8c69706e1312c18f00ff90cd5fa3201
Karolina Zajicova <karolzaic31@gmail.com>
2024-04-24 18:24:49 +0200

    second commit

--- src/army/Main.java
+++ src/army/Main.java
@@ -4,19 +4,16 @@ import army.Soldier.Walkira;
 import army.PrincessArmy;
 
 
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    final Walkira walkira1 = new Walkira ("walkira Elen","magic sword",10);
-    final Walkira walkira2 = new Walkira ( "walkira Moore", "can teleport", 12);
-
-    final PrincessArmy princessArmy = new PrincessArmy();
-    princessArmy.addSoldier
+        final Walkira walkira1 = new Walkira("walkira Elen", "magic sword", 10);
+        final Walkira walkira2 = new Walkira("walkira Moore", "can teleport", 12);
 
 
-
+        final PrincessArmy princessArmy = new PrincessArmy();
+        princessArmy.addSoldier
 
 
     }
+
+}
---
+++ src/army/Soldier/Warrior.java
@@ -0,0 +1,13 @@
+package army.Soldier;
+
+public class Warrior extends Soldier {
+
+    public Warrior (String name, String superPower, int damage){
+        super (name,superPower, damage);
+    }
+
+    @Override
+    public void reportItself() {
+
+    }
+}
